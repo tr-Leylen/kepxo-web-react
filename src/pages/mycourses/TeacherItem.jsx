6ef9916e4260547b916827609ef59ec7864a2eb4
elturanshucai <elturanfcb@gmail.com>
2025-02-26 10:32:11 +0400

    teacher item update

--- src/pages/mycourses/TeacherItem.jsx
+++ src/pages/mycourses/TeacherItem.jsx
@@ -1,18 +1,8 @@
-import React, { useMemo, useState } from 'react'
-import { getUser } from '../../controllers/user.controller.js'
+import React from 'react'
 
 const TeacherItem = ({ teacher }) => {
-    const [data, setData] = useState({})
-
-    const getData = async () => {
-        const res = await getUser(teacher)
-        setData(res)
-    }
-    useMemo(() => {
-        getData()
-    }, [teacher])
     return (
-        <option value={teacher}>{data?.firstName + ' ' + data?.lastName}</option>
+        <option value={teacher._id}>{teacher?.firstName + ' ' + teacher?.lastName}</option>
     )
 }
 
